d3bf2670d60252ab95d5da6ad447af9aafc8000e
[AriSenthil1012] <[arisenthil@diractechs.onmicrosoft.com]>
2025-01-14 03:12:16 -0600

    first commit

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+# Set page title 
+st.set_page_config(page_title="My Streamlit App", layout="wide")
+
+# Add a title 
+st.title("Welcome to my Streamlit app")
+
+# Add some text 
+st.write("This is a simple streamlit application")
